06e17938865c4aae3dc5a410b8688288c611fd65
Brian Awood <bawood@umich.edu>
2022-05-24 14:47:15 -0400

    rename files for wsgi server

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,40 @@
-import os
-import mod_wsgi.server
+from ast import Str
+import json
+from flask import Flask, Response, request
+import socket
+app = Flask(__name__)
 
-mod_wsgi.server.start(
-  '--log-to-terminal',
-  '--port', '8080',
-  '--trust-proxy-header', 'X-Forwarded-For',
-  '--trust-proxy-header', 'X-Forwarded-Port',
-  '--trust-proxy-header', 'X-Forwarded-Proto',
-  '--processes', os.environ.get('MOD_WSGI_PROCESSES', '1'),
-  '--threads', os.environ.get('MOD_WSGI_THREADS', '5'),
-  '--application-type', 'module',
-  '--entry-point', 'app'
-)
+@app.route('/hello')
+def hello_world():
+   return 'Hello World'
+
+@app.route('/emergencycall_alert/', methods = ['POST'])
+def zoomphone_alert():
+   remote=socket.gethostbyaddr(request.remote_addr)
+   token = request.headers.get('authorization', type=str)
+   if token:
+      print("emergency call alert webhook received from: ", remote[0])
+      print("auth token: ", token)
+      if request.is_json:
+         if request.json:
+            print(json.dumps(request.json))
+      return Response("", 200)
+   else:
+      return Response("Access denied", 401)
+
+@app.route('/device_registration/', methods = ['POST'])
+def zoomphone_registration():
+   remote=socket.gethostbyaddr(request.remote_addr)
+   token = request.headers.get('authorization', type=str)
+   if token:
+      print("device registration webhook received from: ", remote[0])
+      print("auth token: ", token)
+      if request.is_json:
+         if request.json:
+            print(json.dumps(request.json))
+      return Response("", 200)
+   else:
+      return Response("Access denied", 401)
+
+if __name__ == '__main__':
+   app.run()
