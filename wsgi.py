d79637f4fd65ea5252d7a0857ffda7eca219d3a3
Brian J Awood <bawood@umich.edu>
2022-10-24 14:05:23 -0400

    catched exception in gethostbyaddr

--- wsgi.py
+++ wsgi.py
@@ -4,16 +4,21 @@ from flask import Flask, Response, request
 import socket
 application = Flask(__name__)
 
+def reverseLookup(IP):
+	try:
+		return socket.gethostbyaddr(IP)[0].lower()
+	except Exception:
+		return (None)
+
 @application.route('/hello')
 def hello_world():
    return 'Hello World'
 
 @application.route('/emergencycall_alert/', methods = ['POST'])
 def zoomphone_alert():
-   remote=socket.gethostbyaddr(request.remote_addr)
    token = request.headers.get('authorization', type=str)
    if token:
-      print("emergency call alert webhook received from: ", remote[0])
+      print("emergency call alert webhook received from: ", reverseLookup(request.remote_addr))
       print("auth token: ", token)
       if request.is_json:
          if request.json:
@@ -24,10 +29,9 @@ def zoomphone_alert():
 
 @application.route('/device_registration/', methods = ['POST'])
 def zoomphone_registration():
-   remote=socket.gethostbyaddr(request.remote_addr)
    token = request.headers.get('authorization', type=str)
    if token:
-      print("device registration webhook received from: ", remote[0])
+      print("device registration webhook received from: ", reverseLookup(request.remote_addr))
       print("auth token: ", token)
       if request.is_json:
          if request.json:
