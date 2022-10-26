44121a445e566030f77c8fcfefdb688f4a9c1d43
Brian J Awood <bawood@umich.edu>
2022-10-26 14:56:19 -0400

    adjust requirements and add env variables

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,13 @@
 from ast import Str
 import json
+from telnetlib import IP
 from flask import Flask, Response, request
+from flask_mysqldb import MySQL
+
 import socket
-application = Flask(__name__)
+app = Flask(__name__)
+
+app.config.from_prefixed_env()
 
 def reverseLookup(IP):
 	try:
@@ -10,35 +15,22 @@ def reverseLookup(IP):
 	except Exception:
 		return IP
 
-@application.route('/hello')
+@app.route('/hello')
 def hello_world():
    return 'Hello World'
 
-@application.route('/emergencycall_alert/', methods = ['POST'])
-def zoomphone_alert():
-   token = request.headers.get('authorization', type=str)
-   if token:
-      print("emergency call alert webhook received from: ", reverseLookup(request.remote_addr))
-      print("auth token: ", token)
-      if request.is_json:
-         if request.json:
-            print(json.dumps(request.json))
-      return Response("", 200)
-   else:
-      return Response("Access denied", 401)
-
-@application.route('/device_registration/', methods = ['POST'])
+@app.route('/device_registration/', methods = ['POST'])
 def zoomphone_registration():
    token = request.headers.get('authorization', type=str)
-   if token:
+   if token == app.config["ZOOM_TOKEN"]:
       print("device registration webhook received from: ", reverseLookup(request.remote_addr))
-      print("auth token: ", token)
       if request.is_json:
          if request.json:
             print(json.dumps(request.json))
       return Response("", 200)
    else:
+      print("invalid auth token: ", token, "from host: ", reverseLookup(request.remote_addr))
       return Response("Access denied", 401)
 
 if __name__ == '__main__':
-   application.run()
+   app.run()
