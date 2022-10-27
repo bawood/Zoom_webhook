0a333deda63534200111e581e7a98ad77d4c0d54
Brian J Awood <bawood@umich.edu>
2022-10-27 12:59:54 -0400

    Timezone from dateutil

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,11 @@
-from ast import Str
 from datetime import datetime
-import json
-from telnetlib import IP
+from dateutil import tz
 from flask import Flask, Response, request
 from flask_mysqldb import MySQL
 
+import json
 import socket
+
 application = Flask(__name__)
 
 application.config.from_prefixed_env()
@@ -39,7 +39,8 @@ def zoomphone_registration():
             device_id = data['payload']['object']['device_id']
             mac_address = data['payload']['object']['mac_address']
             print("got webhook for device_id: ", device_id, " mac_address: ", mac_address)
-            ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            ts = datetime.now(tz=tz.gettz('America/Detroit'))
+            #ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             sql_vals = (device_id, ts ,"ph_" + mac_address + "%")
             #sql = "SELECT * FROM ZoomPhoneNameFloorRoom WHERE PhoneName LIKE %s"
             sql = "UPDATE ZoomPhoneNameFloorRoom SET deviceId = %s, stamp = %s WHERE PhoneName LIKE %s"
