15be66e8c6bc257bec358deb091245fb3d1520fe
Brian J Awood <bawood@umich.edu>
2023-06-20 12:51:24 -0400

    Update to logging, attempt DB insert missing entry

--- wsgi.py
+++ wsgi.py
@@ -3,18 +3,20 @@ from dateutil import tz
 from flask import Flask, Response, request
 from flask_mysqldb import MySQL
 from umichemail import send_mail
+from dotenv import load_dotenv
 
 import json
 import socket
 import logging
 
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-application.config.from_prefixed_env()
-mysql = MySQL(application)
-mail_from = application.config["MAIL_FROM"]
-mail_to = application.config["MAIL_TO"]
+load_dotenv(override=True)
+app.config.from_prefixed_env()
+mysql = MySQL(app)
+mail_from = app.config["MAIL_FROM"]
+mail_to = app.config["MAIL_TO"]
 logging.basicConfig(level=logging.INFO)
 
 
@@ -29,13 +31,13 @@ def test_mysql_connection():
     try:
         conn = mysql.connection.cursor()
         if conn:
-            logging.debug("Mysql test connection successful")
+            app.logger.debug("Mysql test connection successful")
             return True
         else:
-            logging.error("Mysql test connection failed")
+            app.logger.error("Mysql test connection failed")
             return False
     except Exception as e:
-        logging.error("Msql connection test exception {}".format(e))
+        app.logger.error("Msql connection test exception {}".format(e))
         return False
 
 
@@ -45,27 +47,27 @@ def test_mysql_query():
         cursor.execute("SELECT DATABASE()")
         result = cursor.fetchall()
         if result:
-            logging.debug("Mysql %s test query successful", result)
+            app.logger.debug("Mysql %s test query successful", result)
             return True
         else:
-            logging.error("Mysql test query failed")
+            app.logger.error("Mysql test query failed")
             return False
     except Exception as e:
-        logging.error("Msql query test exception %s", e.__cause__)
+        app.logger.error("Msql query test exception %s", e.__cause__)
         return False
 
 
-@application.route('/hello')
+@app.route('/hello')
 def hello_world():
     if request.remote_addr.startswith('67.149'):
-        logging.info("hello request from %s", request.remote_addr)
+        app.logger.info("hello request from %s", request.remote_addr)
 #    send_mail(subject='Ignore: Zoom webhook test message',
 #              message='received a get request for /hello url from host {}'.format(reverseLookup(request.remote_addr)),
 #              from_address=mail_from, to_address=mail_to)
     return 'Hello World'
 
 
-@application.route('/health')
+@app.route('/health')
 def test_mysql():
     if test_mysql_connection() and test_mysql_query():
         return "MySQL connection and query test successful."
@@ -73,50 +75,57 @@ def test_mysql():
         return "MySQL connection or query test failed.", 500
 
 
-@application.route('/device_registration/', methods=['POST'])
+@app.route('/device_registration/', methods=['POST'])
 def zoomphone_registration():
     token = request.headers.get('authorization', type=str)
-    if token == application.config["ZOOM_TOKEN"]:
-        logging.debug("device registration webhook received from: ",
+    if token == app.config["ZOOM_TOKEN"]:
+        app.logger.debug("device registration webhook received from: %s",
                      reverseLookup(request.remote_addr))
+        app.logger.debug("%s", request.data)
         if request.is_json:
             data = request.get_json()
             if data:
                 device_id = data['payload']['object']['device_id']
                 mac_address = data['payload']['object']['mac_address']
-                logging.info("got webhook for device_id: {} mac_address: {}".format(
+                app.logger.info("got webhook for device_id: {} mac_address: {}".format(
                     device_id, mac_address))
 
                 ts = datetime.now(tz=tz.gettz('America/Detroit'))
                 sql_vals = (device_id, ts.strftime(
                     '%Y-%m-%d %H:%M:%S'), "ph_" + mac_address + "%")
                 sql = "UPDATE ZoomPhoneNameFloorRoom SET deviceId = %s, stamp = %s WHERE PhoneName LIKE %s"
-                logging.debug(
+                app.logger.debug(
                     'Attempting mysql update with: {}'.format(sql_vals))
                 try:
                     cur = mysql.connection.cursor()
                     if not cur:
                         msg = 'Unable to connect to MySQL DB: {}:{}'.format(
-                            application.config["MYSQL_HOST"], application.config["MYSQL_DB"])
-                        logging.error(msg)
+                            app.config["MYSQL_HOST"], app.config["MYSQL_DB"])
+                        app.logger.error(msg)
                         send_mail(message=msg, subject='ERROR: Zoom webhook Mysql connection',
                                   from_address=mail_from, to_address=mail_to)
                         return Response("Database connection failed", headers='Retry-After: 300', status=503)
                     else:
                         result = cur.execute(sql, sql_vals)
                         mysql.connection.commit()
-                        cur.close()
                         if result == 0:
-                            msg = 'Manual work is likely required since a webhook was received but no rows in DB were updated for phone with MAC: {}'.format(
+                            sql = "INSERT INTO ZoomPhoneNameFloorRoom ( deviceId, stamp ) VALUES(%s, %s);"
+                            sql_vals = (device_id, ts.strftime('%Y-%m-%d %H:%M:%S'))
+                            result = cur.execute(sql, sql_vals)
+                            mysql.connection.commit()
+                            app.logger.debug("SQL insert result: %s", result)
+                            msg = 'device registration webhook was received but no existing entries in DB matched for phone with MAC: {}'.format(
                                 mac_address)
-                            logging.error(msg)
-                            send_mail(message=msg, subject='Notice: Zoom webhook, but no DB entry updated',
+                            app.logger.error(msg)
+                            send_mail(message=msg, subject='Notice: Zoom device registration, missing DB entry',
                                       from_address=mail_from, to_address=mail_to)
+                    cur.close()
                 except Exception as e:
                     mysql.connection.rollback()
+                    cur.close()
                     send_mail(message=e, subject='SQL Exception occurred',
                               from_address=mail_from, to_address=mail_to)
-                    logging.error("SQL Exception occurred: ", e)
+                    app.logger.error("SQL Exception occurred: ", e)
         return Response("", 200)
     else:
         print("invalid auth token: ", token, "from host: ",
@@ -125,4 +134,4 @@ def zoomphone_registration():
 
 
 if __name__ == '__main__':
-    application.run()
+    app.run()
